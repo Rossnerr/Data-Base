5845aea57986469313e67853915abba7ca8ee08e
Rossnerr <metei1997vasile@gmail.com>
2018-10-30 00:04:29 +0200

    Delete Task3.sql

--- DB_Lab.4/Querries/Task3.sql
+++
@@ -1,9 +0,0 @@
-USE universitatea
-
-SELECT DISTINCT Nume_Student, Prenume_Student
-FROM studenti
-INNER JOIN studenti_reusita 
-ON studenti.Id_Student = studenti_reusita.Id_Student
-WHERE NOT studenti.Id_Student = Any (SELECT Id_Student
-									 FROM studenti_reusita
-									 WHERE Tip_Evaluare = 'Reusita curenta' and Nota > 5)
