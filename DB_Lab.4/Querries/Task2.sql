98a20c1bde55e627ebf006daf5a9027e883df217
Rossnerr <metei1997vasile@gmail.com>
2018-10-30 00:04:22 +0200

    Delete Task2.sql

--- DB_Lab.4/Querries/Task2.sql
+++
@@ -1,6 +0,0 @@
-USE universitatea
-SELECT Nume_Profesor, Prenume_Profesor, COUNT(DISTINCT Id_Disciplina) AS Nr_Discipline_Predate
-FROM dbo.studenti_reusita
-INNER JOIN dbo.profesori
-ON dbo.studenti_reusita.Id_Profesor=dbo.profesori.Id_Profesor
-GROUP BY Nume_Profesor, Prenume_Profesor
