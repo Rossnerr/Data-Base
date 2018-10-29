39de4d9d96620aae1dee42dc49633ab8e05ad839
Rossnerr <metei1997vasile@gmail.com>
2018-10-30 00:04:13 +0200

    Delete Task1.sql

--- DB_Lab.4/Querries/Task1.sql
+++
@@ -1,8 +0,0 @@
-USE universitatea
-SELECT Nume_Student, Prenume_Student
-FROM dbo.studenti
-INNER JOIN dbo.studenti_reusita
-ON dbo.studenti.Id_Student = dbo.studenti_reusita.Id_Student
-INNER JOIN dbo.discipline
-ON dbo.studenti_reusita.Id_Disciplina = dbo.discipline.Id_Disciplina
-WHERE Tip_Evaluare='Examen' AND Data_Evaluare LIKE '2018-%-%' AND Nota BETWEEN 4 AND 8 AND Disciplina='Baze de date'
